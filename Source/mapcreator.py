821e1c17e859382506e1daa96f460a5b9355a10d
Dewi-Payne <40428706+Dewi-Payne@users.noreply.github.com>
2019-11-08 09:48:35 +0000

    Adds map creator script in python
    This is to be used to easily create the map code for creating a map for the game in the current format to make the creation easier for the programmer.

---
+++ Source/mapcreator.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+root = tk.Tk()
+sq_size = 60
+
+m = []
+d = {}
+layers = {0:"#cdcdcd", #gray
+          1:"#0021ff", #blue
+          2:"#fff100", #yellow
+          3:"#ff0000", #red
+          4:"#747474"} #dark gray
+current_colour = 0
+def set_current_colour(c):
+    global current_colour
+    print (current_colour,c)
+    
+    current_colour = c
+    
+    
+def colchange(self):
+    col = self.cget("bg")
+    global current_colour
+    self.setbg(current_colour)
+
+class square(tk.Canvas):    
+    def __init__(self,x,y,ID):
+        super().__init__(root,height=10,
+                           width=10,bg="grey")
+        tk.Canvas.grid(self,row=x,column=y)
+        self.config(bd=0,highlightthickness=0, relief='ridge')
+        self.bind("<Enter>", (lambda event: colchange(self)))
+        self.backg = self.cget("bg")
+        backg_c = 0
+    def setbg(self,col):
+        self.config(bg=layers[col])
+        self.backg=layers[col]
+        current_colour = col
+
+
+def p(d):
+    for i in range(len(d)):
+        if i%sq_size==0:
+            print("")
+        if d[i].backg == "blue":
+            print(1, end=", ")
+        else:
+            print(0, end=", ")
+
+for x in range(sq_size):
+    for y in range(sq_size):
+        ID = (x*sq_size)+y
+        d[ID] = square(x,y,ID)
+        this_o = d[ID]
+
+outb = tk.Button(root,command = (lambda : p(d)))
+outb.grid(row=61,column=61)
+
+but_1 = tk.Button(root,text="lg",command = (lambda : set_current_colour(0)))
+but_1.grid(row=61,column = 0, columnspan = 10)
+
+but_2 = tk.Button(root,text="bl",command = (lambda : set_current_colour(1)))
+but_2.grid(row=61,column=11,columnspan = 10)
+
+but_2 = tk.Button(root,text="ye",command = (lambda : set_current_colour(2)))
+but_2.grid(row=61,column=21,columnspan = 10)
+
+but_2 = tk.Button(root,text="rd",command = (lambda : set_current_colour(3)))
+but_2.grid(row=61,column=31,columnspan = 10)
+
+but_2 = tk.Button(root,text="dg",command = (lambda : set_current_colour(4)))
+but_2.grid(row=61,column=41,columnspan = 10)
