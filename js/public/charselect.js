030714db7291d2012a1e3910955de6320890e786
jw147 <jw147@hw.ac.uk>
2019-11-14 21:45:28 +0000

    add character select page

---
+++ js/public/charselect.js
@@ -0,0 +1,68 @@
+//Function to get the mouse position
+function getMousePos(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+}
+//Function to check whether a point is inside a rectangle
+function isInside(pos, rect){
+    return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y
+}
+
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+//The rectangle should have x,y,width,height properties
+var archer = {
+    x:0,
+    y:0,
+    width:200,
+    height:600
+};
+
+var knight = {
+    x:200,
+    y:0,
+    width:200,
+    height:600
+};
+
+var mage = {
+    x:400,
+    y:0,
+    width:200,
+    height:600
+};
+
+
+var img = new Image();
+img.onload = function () {
+    context.drawImage(img, 0, 0);
+}
+img.src = "charselect.png";
+
+//Binding the click event on the canvas
+canvas.addEventListener('click', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+
+    if (isInside(mousePos,archer)) {
+        window.open("index.html");
+    }
+}, false);
+
+canvas.addEventListener('click', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+
+    if (isInside(mousePos,knight)) {
+        window.open("index.html");
+    }
+}, false);
+
+canvas.addEventListener('click', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+
+    if (isInside(mousePos,mage)) {
+        window.open("index.html");
+    }
+}, false);
