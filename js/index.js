8c5a76b1b39424c316802753809f565427875257
jw147 <jw147@hw.ac.uk>
2019-11-14 23:17:46 +0000

    changing text

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen(3000, () => console.log('Moonscape is now running at localhost:3000!'));
 app.use(express.static('public'));
 
